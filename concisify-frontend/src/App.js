26c580b5d6feb5a187ba5d6ff698df5e1810a566
script-jpg <n45jiang@uwaterloo.ca>
2021-01-16 21:54:25 -0500

    Updated main page

--- concisify-frontend/src/App.js
+++ concisify-frontend/src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
 import navbar from './resources/images/Navbar.png'
+import clock from './resources/images/Clock_Lines.png'
 
 
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)-20
+const vw = window.innerWidth -20
 function App() {
   return (
     <div id="container">
-      <img src={navbar} height="79px" width={`${vw}px`}/>
+      <img src={navbar} height="79px" width={`${vw}px`} style={{paddingBottom: 140}}/>
+      <div id="body">
+        <p id="header">Concisify</p>
+        <img src={clock} height="101px" width="101px" style={{paddingTop:69, paddingLeft:14}}/>
+      </div>
+      <div id="body" style={{paddingTop: 1}}>
+        <p id="desc">The notes that take you hours, printed in seconds with AI!</p>
+      </div>
+      
     </div>
   );
 }
