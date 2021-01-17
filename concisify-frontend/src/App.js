40e9c878cda6d116d13afff8d8c20a2465694b52
script-jpg <n45jiang@uwaterloo.ca>
2021-01-16 20:26:30 -0500

    added frontend start files

---
+++ concisify-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <ul>
+      <li>I like apples 🍎</li>
+      <li>I like anime 📺</li>
+      <li>I like movies 🎬</li>
+    </ul>
+  );
+}
+
+export default App;
