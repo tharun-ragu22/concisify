1bd87a3f2ad9583bda692e1def58106cebb9e8a8
script-jpg <n45jiang@uwaterloo.ca>
2021-01-16 21:00:01 -0500

    changes to frontend

--- concisify-frontend/src/App.js
+++ concisify-frontend/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import navbar from './resources/images/Navbar.png'
+
+
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)-20
 function App() {
   return (
-    <ul>
-      <li>I like apples 🍎</li>
-      <li>I like anime 📺</li>
-      <li>I like movies 🎬</li>
-    </ul>
+    <div id="container">
+      <img src={navbar} height="79px" width={`${vw}px`}/>
+    </div>
   );
 }
 
